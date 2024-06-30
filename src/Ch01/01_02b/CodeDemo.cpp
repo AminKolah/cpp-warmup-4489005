1dff1bbd6997f586581bedbeb03de9775de548e7
Amin Kolahdouz <me.kolahdouz@gmail.com>
2024-06-30 21:13:13 +0000

    commit sth

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Learning C++ 
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by AminKolah 
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string str;
+    std::cin >> str;
+    std::cout << str << std::endl;
+    std::cout << std::endl << std::endl;
+    return(0);
+}
